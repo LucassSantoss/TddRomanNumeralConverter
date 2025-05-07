c94beec17d5e54e9c0e5bbe6a2bee72459d66afd
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-05-06 22:50:54 -0300

    test: implement test to convert V to 5  - use setup function to initialize SUT

--- src/test/java/org/example/RomanNumeralConverterTest.java
+++ src/test/java/org/example/RomanNumeralConverterTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,11 +8,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumeralConverterTest {
+    private RomanNumeralConverter sut;
+
+    @BeforeEach
+    public void setUp() {
+        sut = new RomanNumeralConverter();
+    }
+
+
     @Test
     @DisplayName("Should convert I to 1")
     void shouldConvertITo1(){
-        RomanNumeralConverter sut = new RomanNumeralConverter();
         int number = sut.convert("I");
         assertThat(number).isEqualTo(1);
+    }
+    
+    @Test
+    @DisplayName("Should convert V to 5")
+    void shouldConvertVTo5(){
+        int number = sut.convert("V");
+        assertThat(number).isEqualTo(5);
     }
 }
