71a8d4c6e3c0fddafa628e025e3302f7214d5f10
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-05-06 23:16:28 -0300

    feat: implement convert roman number with 2 characters

--- src/main/java/org/example/RomanNumeralConverter.java
+++ src/main/java/org/example/RomanNumeralConverter.java
@@ -3,13 +3,17 @@ package org.example;
 import java.util.Map;
 
 public class RomanNumeralConverter {
-    private static final Map<String, Integer> table = Map.of(
-            "I", 1, "V", 5, "X", 10, "L", 50,
-            "C", 100, "D", 500, "M", 1000
+    private static final Map<Character, Integer> table = Map.of(
+        'I', 1,'V', 5, 'X', 10,'L', 50,
+        'C', 100,'D', 500, 'M', 1000
     );
 
 
     public int convert(String numberInRoman){
-        return table.get(numberInRoman);
+        int finalNumber = 0;
+        for(int i = 0; i < numberInRoman.length(); i++) {
+            finalNumber += table.get(numberInRoman.charAt(i));
+        }
+        return finalNumber;
     }
 }
