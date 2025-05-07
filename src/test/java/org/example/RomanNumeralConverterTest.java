57375102e53dc098be5b8aa07709716177dffa81
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-05-06 22:45:57 -0300

    build: base project

---
+++ src/test/java/org/example/RomanNumeralConverterTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanNumeralConverterTest {
+  @org.junit.jupiter.api.Test
+  @org.junit.jupiter.api.DisplayName("Test name")
+  void testName(){
+
+      org.junit.jupiter.api.Assertions.fail();
+  }
+}
