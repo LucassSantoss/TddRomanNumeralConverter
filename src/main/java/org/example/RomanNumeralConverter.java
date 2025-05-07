3fcff29b9ee6da15c1c37f2b37376adb170221d8
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-05-06 23:11:42 -0300

    feat: implement all basic convertions from roman to arabic

--- src/main/java/org/example/RomanNumeralConverter.java
+++ src/main/java/org/example/RomanNumeralConverter.java
@@ -4,8 +4,8 @@ import java.util.Map;
 
 public class RomanNumeralConverter {
     private static final Map<String, Integer> table = Map.of(
-            "I", 1,
-            "V", 5
+            "I", 1, "V", 5, "X", 10, "L", 50,
+            "C", 100, "D", 500, "M", 1000
     );
 
 
